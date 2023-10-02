c79dc83dfba10326952f0f0187a23ff291fa7579
Joshua Sahagun <joshuasahagun1996@yahoo.com>
2023-10-02 15:15:55 -0700

    edited counter according to flake

--- src/counter.py
+++ src/counter.py
@@ -11,13 +11,14 @@ COUNTERS = {}
 # let Flask know that the only methods that is allowed to called
 # on this function is "POST".
 
+
 @app.route('/counters/<name>', methods=['POST'])
 def create_counter(name):
     """Create a counter"""
     app.logger.info(f"Request to create counter: {name}")
     global COUNTERS
     if name in COUNTERS:
-        return {"Message":f"Counter {name} already exists"}, status.HTTP_409_CONFLICT
+        return {"Message": f"Counter {name} already exists"}, status.HTTP_409_CONFLICT
 
     COUNTERS[name] = 0
     return {name: COUNTERS[name]}, status.HTTP_201_CREATED
@@ -30,13 +31,13 @@ def update_counter(name):
     # Increment the counter by 1
     COUNTERS[name] += 1
     # Return the new counter and a 200_OK return code
-    return{ name: COUNTERS[name] }, status.HTTP_200_OK
+    return {name: COUNTERS[name]}, status.HTTP_200_OK
 
 
 @app.route('/counters/<name>', methods=['GET'])
 def get_counter(name):
     if name in COUNTERS:
-        return{ name: COUNTERS[name] }, status.HTTP_200_OK
+        return {name: COUNTERS[name]}, status.HTTP_200_OK
     else:
         return {}, status.HTTP_404_NOT_FOUND
 
